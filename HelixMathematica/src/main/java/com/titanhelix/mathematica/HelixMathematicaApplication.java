9e7e396eab94c9c34b812b21a0ee16368995fd2b
devil5 <m.momin.farooqi@gmail.com>
2017-12-12 21:55:35 +1100

    Revisions:
    1. Deployable .WAR

--- HelixMathematica/src/main/java/com/titanhelix/mathematica/HelixMathematicaApplication.java
+++ HelixMathematica/src/main/java/com/titanhelix/mathematica/HelixMathematicaApplication.java
@@ -2,9 +2,17 @@ package com.titanhelix.mathematica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HelixMathematicaApplication {
+public class HelixMathematicaApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HelixMathematicaApplication.class);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(HelixMathematicaApplication.class, args);
 	}
